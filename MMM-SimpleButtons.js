7b7d4faeef0a2b0684058567b0140d9a58079945
Michael Marschner <michael.marschner@freequenzart.de>
2018-06-21 15:45:11 +0200

    code cleaning

--- MMM-SimpleButtons.js
+++ MMM-SimpleButtons.js
@@ -9,11 +9,11 @@
 Module.register('MMM-SimpleButtons',{	
 	requiresVersion: "2.1.0",
 	defaults: {
-				clickDelay: 2000,
-			    buttons: {
-					17: "BUTTON_17_CLICKED",
-					27: "BUTTON_27_CLICKED"
-				}	
+		clickDelay: 2000,
+		buttons: {
+			17: "BUTTON_17_CLICKED",
+			27: "BUTTON_27_CLICKED"
+		}	
 	},	
 	// Override socket notification handler.
 	socketNotificationReceived: function(notification, payload) {
